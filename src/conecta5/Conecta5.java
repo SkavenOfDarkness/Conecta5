fef59dc9c2bec84a9b762ef0f53dc176b85019ae
lluis <lluisgalera@gmail.com>
2014-05-14 18:51:58 +0200

    Funcionalidad click poner ficha (un color) funcionando

---
+++ src/conecta5/Conecta5.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package conecta5;
+
+import java.awt.Component;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Lluis
+ */
+public class Conecta5 extends JFrame implements MouseListener {
+
+    /**
+     * @param args the command line arguments
+     */
+    Tablero tablero;
+    
+    //Programa principal, llama al constructor del juego y hace visible la ventana
+    public static void main(String[] args) {
+        Conecta5 juego = new Conecta5();
+        juego.setVisible(true);
+    }
+    
+    public Conecta5() {
+        super("Conecta 5");
+        initComponents();
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void initComponents() {
+        tablero = new Tablero();
+//        tablero.Pon(Pieza.ROJA, 0, 0);
+//        tablero.Pon(Pieza.AZUL, 1, 0);
+        tablero.addMouseListener(this);
+        this.getContentPane().add(tablero);
+        this.setSize(tablero.getPreferredSize());
+        this.pack();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        int x = 0, y = 0;
+        Component comp = null;
+        if(e.getButton() == MouseEvent.BUTTON1) {
+            x = e.getX()/54;
+            y = e.getY()/54;
+            comp = e.getComponent();
+        }
+//        System.out.println("X: " + x + " | Y: " + y + "         " + comp);
+        tablero.Pon(Pieza.ROJA, y, x);
+        repaint();
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+}
